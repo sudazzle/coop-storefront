13babbd0049571790eccecb2711447d40fb0174d
Sudaman Shrestha <sudaman@wanda.space>
2024-04-16 20:56:07 +0200

    fix: serve page with mock data turned on and fix playwright

--- e2e-tests/site.spec.ts
+++ e2e-tests/site.spec.ts
@@ -2,11 +2,11 @@ import { test, expect,  } from '@playwright/test';
 
 test('Test if page elements exists', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  await page.waitForTimeout(20000);
-  await expect(page.getByText('Extra, Sarpsborg')).toBeVisible();
+  await expect(page.getByText('Coop.no')).toBeVisible();
   await expect(page.getByText('Min Side')).toBeVisible();
   await expect(page.getByText('Medlem')).toBeVisible();
   await expect(page.getByText('Coop Kjeder')).toBeVisible();
+  await expect(page.getByText('Våre Butikker')).toBeVisible();
   await expect(page.getByRole('button', { name: 'Sorter etter Nærmeste' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'Sorter etter Fjerneste' })).toBeVisible();
 });
--- pages/api/get-stores.ts
+++ pages/api/get-stores.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
+// import axios from 'axios';
 import { storesJson } from './store-json';
  
 export default async function handler(
@@ -7,9 +7,10 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const response = await axios.get('https://minside.coop.no/StoreService/SearchStores');
-    res.status(200).json(response.data);
+    res.status(200).json(storesJson);
+    // const response = await axios.get('https://minside.coop.no/StoreService/SearchStores');
+    // res.status(200).json(response.data);
   } catch (err) {
-    return res.status(200).json(storesJson);
+    res.status(200).json(storesJson);
   }
 }
