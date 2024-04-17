8c04e5f0cd2c82165e26030db11dffd0fa8cd1d5
Sudaman Shrestha <sudaman@wanda.space>
2024-04-17 22:08:03 +0200

    fix: bring back reuseExisingServer in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,9 +70,9 @@ export default defineConfig({
     // },
   ],
 
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
